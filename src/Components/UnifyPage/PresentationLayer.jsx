b71209bf4286a4721088de1cb85734e2c1a3799b
Gaurav Verma <briskgaurav@gmail.com>
2025-09-18 13:36:13 +0530

    update - [Gaurav]
    PresentationDiagram

--- src/Components/UnifyPage/PresentationLayer.jsx
+++ src/Components/UnifyPage/PresentationLayer.jsx
@@ -1,114 +1,80 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect } from "react";
-
-
-const usecaseLayer = [
-    "Customer Persistency",
-    "Fraud Detection",
-    "Demand Forecasting",
-    "Document Search",
-    "Content/Code Generation",
-    "Information Extraction",
-  ];
-
-  const coreTools = [
-    {
-      name: "SmartEDA",
-      src: "/assets/icons/presentationLayer/left/smartEDATop.svg",
-    },
-    {
-      name: "FeatureCraft",
-      src: "/assets/icons/presentationLayer/left/featureCraft.svg",
-    },
-    {
-      name: "ModelCraft",
-      src: "/assets/icons/presentationLayer/left/modelCraft.svg",
-    },
-  ];
-
-  const capabilities = [
-    {
-      name: "Data Integration Toolkit",
-      src: "/assets/icons/presentationLayer/left/dataIntegration.svg",
-    },
-    {
-      name: "Feature Store",
-      src: "/assets/icons/presentationLayer/left/featureStore.svg",
-    },
-    {
-      name: "Model Development",
-      src: "/assets/icons/presentationLayer/left/modelDevelopment.svg",
-    },
-    {
-      name: "SmartEDA",
-      src: "/assets/icons/presentationLayer/left/smartEDA.svg",
-    },
-    {
-      name: "Model Management & Monitoring",
-      src: "/assets/icons/presentationLayer/left/modelManagemnt.svg",
-    },
-    {
-      name: "Feedback Loop",
-      src: "/assets/icons/presentationLayer/left/feedbackLoop.svg",
-    },
-  ];
-
-  const genAITop = [
-    {
-      name: "LLM Hub",
-      src: "/assets/icons/presentationLayer/right/llm.svg",
-    },
-    {
-      name: "Prompt Management",
-      src: "/assets/icons/presentationLayer/right/promptManagement.svg",
-    },
-    {
-      name: "Agent Management",
-      src: "/assets/icons/presentationLayer/right/agent.svg",
-    },
-    {
-      name: "Tool Hub",
-      src: "/assets/icons/presentationLayer/right/toolHUB1.svg",
-    },
-  ];
-
-  const genAIBottom = [
-    {
-      name: "LLM Deployment",
-      src: "/assets/icons/presentationLayer/right/llmdeployment.svg",
-    },
-    {
-      name: "Tool Management",
-      src: "/assets/icons/presentationLayer/right/tool.svg",
-    },
-    {
-      name: "Vector DB",
-      src: "/assets/icons/presentationLayer/right/vector.svg",
-    },
-    {
-      name: "Data Indexing",
-      src: "/assets/icons/presentationLayer/right/dataindex.svg",
-    },
-    {
-      name: "Orchestrator",
-      src: "/assets/icons/presentationLayer/right/orchestrator.svg",
-    },
-    {
-      name: "Fine-Tuning",
-      src: "/assets/icons/presentationLayer/right/finetuning.svg",
-    },
-    {
-      name: "Guardrails",
-      src: "/assets/icons/presentationLayer/right/guardrails.svg",
-    },
-  ];
-
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 export default function PresentationLayer() {
-  
+  const presentationLayerRef = useRef(null);
+  const UseCaseRef = useRef(null);
+  const AIStudioRef = useRef(null);
+  const GenAIStudioRef = useRef(null);
+  const UnifyRef = useRef(null);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top 120%",
+          end: "50% 50%",
+          scrub: true,
+          markers:true,
+        },
+      });
+
+      tl.from(
+        presentationLayerRef.current,
+        {
+          rotationX: "45",
+          y: -25
+        },
+        0
+      )
+        .from(
+          UseCaseRef.current,
+          {
+            rotationX: "45",
+            y: -25,
+          },
+          0
+        )
+        .from(
+          AIStudioRef.current,
+          {
+            rotationX: "50",
+            y: -80,
+            x: -50,
+          },
+          0
+        )
+        .from(
+          GenAIStudioRef.current,
+          {
+            rotationX: "50",
+            y: -80,
+            x: 50,
+          },
+          0
+        )
+        .from(
+          UnifyRef.current,
+          {
+            rotationX: "45",
+            y: -25,
+          },
+          0
+        );
+    }, sectionRef);
+
+    return () => ctx.revert();
+  }, []);
+
   const ToolItem = ({ tool }) => (
-    <div className="flex flex-shrink-0 items-center w-[15%] max-sm:w-[22%] justify-center flex-col space-y-[1vw] text-center  h-auto">
-      <div className="w-[4vw] max-sm:w-[25vw] h-auto">
+    <div className="flex flex-shrink-0 max-sm:w-[25%] items-center w-[15%] justify-center flex-col space-y-[1vw] text-center h-auto">
+      <div className="w-[4vw] max-sm:w-[10vw] h-auto">
         <Image
           width={100}
           height={100}
@@ -117,23 +83,38 @@ export default function PresentationLayer() {
           alt={tool.name}
         />
       </div>
-      <p className="text-[.9vw] text-white-300 max-sm:text-[2.5vw] w-[180%]">{tool.name}</p>
+      <p className="text-[.9vw] max-sm:text-[2.8vw] text-white-300 max-sm:w-full w-[180%]">
+        {tool.name}
+      </p>
     </div>
   );
 
   return (
-    <section id="presentationLayer" className="h-fit container overflow-x-scroll flex-col fadeup gap-[.8vw] max-sm:gap-[4vw] w-[100%] mx-auto max-sm:items-start items-center justify-center text-center flex ">
-      <div className="max-sm:w-[450vw] max-sm:py-[4vw]  w-full py-[.7vw]  border-white/20 text-[1.5vw] rounded-full border bg-gradient-to-r from-white/0 to-white/6">
-        <p className="text-[1.5vw] max-sm:text-[5vw]">Presentation Layer</p>
+    <section
+      ref={sectionRef}
+      style={{
+        perspective: "1000px",
+      }}
+      id="presentationLayer"
+      className="h-fit container overflow-x-scroll flex-col fadeup gap-[.8vw] max-sm:gap-[3vw] w-[100%] mx-auto items-center justify-center text-center flex "
+    >
+      <div
+        ref={presentationLayerRef}
+        className="w-full max-sm:py-[1.5vw] py-[.7vw] max-sm:rounded-[3vw]  border-white/20 text-[1.5vw] rounded-full border bg-gradient-to-r from-white/0 to-white/6"
+      >
+        <p className="text-[1.5vw]  max-sm:text-[4.5vw]">Presentation Layer</p>
       </div>
 
-      <div className=" space-y-[1vw] max-sm:space-y-[4vw] max-sm:w-[450vw] px-[1vw] max-sm:px-[4vw] max-sm:py-[6vw] w-full py-[1vw] border-white/20 rounded-[2vw] max-sm:rounded-[5vw] border bg-gradient-to-r from-white/0 to-white/6">
-        <p className="text-[1.5vw] max-sm:text-[5vw]">Use Cases</p>
-        <div className="flex gap-[1vw] max-sm:gap-[4vw] items-center justify-evenly">
+      <div
+        ref={UseCaseRef}
+        className="space-y-[1vw] max-sm:space-y-[2vw] px-[1vw] w-full py-[1vw] border-white/20 max-sm:px-[3vw] max-sm:py-[3vw] rounded-[2vw] max-sm:rounded-[4vw] border bg-gradient-to-r from-white/0 to-white/6"
+      >
+        <p className="text-[1.5vw] max-sm:text-[4.5vw]">Use Cases</p>
+        <div className="flex max-sm:gap-[2vw] gap-[1vw] max-sm:flex-col items-center justify-evenly">
           {usecaseLayer.map((text, index) => (
             <p
               key={index}
-              className="w-fit py-[1vw] px-[1.6vw] max-sm:w-full text-[1vw] bg-gradient-to-r from-white/2 to-white/8 border-white/20 rounded-[1vw] border max-sm:py-[8vw]  max-sm:rounded-[5vw] max-sm:text-[4vw] text-white-300"
+              className="w-fit max-sm:w-full max-sm:text-[3.5vw] py-[1vw] px-[1.6vw] text-[1vw] bg-gradient-to-r from-white/2 to-white/8 border-white/20 rounded-[1vw] max-sm:rounded-[1.2vw] max-sm:py-[1.5vw] border text-white-300"
             >
               {text}
             </p>
@@ -141,13 +122,13 @@ export default function PresentationLayer() {
         </div>
       </div>
 
-      <div className="h-fit flex gap-[.8vw] max-sm:gap-[4vw] max-sm:w-[450vw] w-full">
-        <div className="h-full overflow-hidden ">
-          <div className="h-fit p-[1.5vw]  max-sm:px-[5vw] max-sm:py-[5vw] text-center w-full bg-gradient-to-r from-white/8 to bg-white/0  border-white/20 rounded-[2vw] max-sm:rounded-[5vw] flex flex-col items-center gap-[.5vw] border">
+      <div className="h-fit flex max-sm:flex-col max-sm:gap-[3vw] gap-[.8vw] w-full">
+        <div ref={AIStudioRef} className="h-full overflow-hidden ">
+          <div className="h-fit p-[1.5vw] text-center w-full bg-gradient-to-r from-white/8 to bg-white/0 border-white/20 rounded-[2vw] max-sm:rounded-[4vw] flex flex-col max-sm:p-[4vw] items-center gap-[.5vw] border">
             <div className="w-full h-full">
-              <p className="text-[1.5vw] ">AI Studio</p>
-              <div className="py-[1.8vw]  border-b relative border-white/20 flex justify-evenly items-center w-full pl-[5vw]">
-                <p className="text-content absolute top-1/2 left-[-5%] max-sm:left-[0%] translate-y-[-50%] rotate-[-90deg]">
+              <p className="text-[1.5vw] max-sm:text-[4vw] ">AI Studio</p>
+              <div className="py-[1.8vw] max-sm:py-[6vw] border-b relative border-white/20 flex justify-evenly items-center w-full pl-[5vw]">
+                <p className="text-content absolute top-1/2 max-sm:left-[-10%] max-sm:!text-[4.2vw] left-[-5%] translate-y-[-50%] rotate-[-90deg]">
                   Core Tools
                 </p>
                 {coreTools.map((tool, index) => (
@@ -156,12 +137,12 @@ export default function PresentationLayer() {
               </div>
             </div>
 
-            <div className="w-full  h-full">
-              <div className="py-[1vw] flex-nowrap relative flex justify-evenly items-start w-full max-sm:py-[8vw] pl-[5vw] max-sm:pl-[20vw]">
-                <p className="text-content absolute top-1/2 max-sm:left-[0%] left-[-5%] translate-y-[-50%] rotate-[-90deg]">
+            <div className="w-full h-full">
+              <div className="py-[1vw] max-sm:py-[6vw] flex-nowrap relative flex justify-evenly items-start w-full pl-[5vw]">
+                <p className="text-content absolute top-1/2 max-sm:left-[-10%] max-sm:!text-[4.2vw] left-[-5%] translate-y-[-50%] rotate-[-90deg]">
                   Capabilities
                 </p>
-                <div className="flex-row flex-wrap justify-center gap-x-[5vw] gap-[0.5vw] max-sm:gap-[5vw] flex w-full items-center">
+                <div className="flex-row flex-wrap justify-center gap-x-[5vw] gap-[0.5vw] flex w-full items-center">
                   {capabilities.map((tool, index) => (
                     <ToolItem key={index} tool={tool} />
                   ))}
@@ -171,27 +152,27 @@ export default function PresentationLayer() {
           </div>
         </div>
 
-        <div className="h-full w-full">
-          <div className="h-fit  bg-gradient-to-l from-white/8 to bg-white/0 p-[1.5vw] max-sm:p-[5vw] max-sm:rounded-[5vw]  text-center w-full  border-white/20 rounded-[2vw] flex flex-col items-center gap-[.5vw] border">
+        <div ref={GenAIStudioRef} className="h-full w-full">
+          <div className="h-fit bg-gradient-to-l from-white/8 to bg-white/0 p-[1.5vw] max-sm:p-[4vw] text-center w-full border-white/20 max-sm:rounded-[4vw] rounded-[2vw] flex flex-col items-center gap-[.5vw] border">
             <div className="w-full h-full">
-              <p className="text-[1.5vw]">Gen AI Studio</p>
-              <div className="py-[1.8vw] border-b relative border-white/20 flex justify-between items-center  w-full max-sm:px-[5vw] px-[3vw] pr-[5vw] max-sm:pr-[25vw]">
-                <p className="text-content  absolute top-1/2 max-sm:right-0 right-[-5%] translate-y-[-50%] rotate-[90deg]">
+              <p className="text-[1.5vw] max-sm:text-[4vw]">Gen AI Studio</p>
+              <div className="py-[1.8vw] max-sm:py-[6vw] border-b relative border-white/20 flex justify-between max-sm:items-start items-center w-full px-[3vw] max-sm:px-0 max-sm:pr-[13vw] pr-[5vw]  max-sm:gap-[3vw]">
+                <p className="text-content absolute top-1/2 right-[-5%] max-sm:right-[-13%] translate-y-[-50%] max-sm:!text-[4.2vw] rotate-[90deg]">
                   Core Tools
                 </p>
-                
+
                 {genAITop.map((tool, index) => (
                   <ToolItem key={index} tool={tool} />
                 ))}
               </div>
             </div>
 
-            <div className="w-full flex items-start max-sm:items-start justify-center max-sm:py-[5vw] h-fit">
-              <div className="py-[1.4vw] flex-nowrap relative flex justify-evenly items-center w-full pr-[5vw] max-sm:pr-[20vw]">
-                <p className="text-content absolute top-1/2 max-sm:right-0 right-[-5%] translate-y-[-50%] rotate-[90deg]">
+            <div className="w-full flex items-start justify-center h-fit">
+              <div className="py-[1.4vw] max-sm:w-full max-sm:py-[6vw] flex-nowrap relative flex justify-evenly items-center w-full max-sm:pr-0 pr-[5vw]">
+                <p className="text-content absolute top-1/2 max-sm:!text-[4.2vw] right-[-5%] max-sm:right-[-15%] translate-y-[-50%] rotate-[90deg]">
                   Capabilities
                 </p>
-                <div className="flex-row flex-wrap justify-center gap-x-[4vw] gap-[2vw] max-sm:gap-y-[8vw] flex w-full items-center">
+                <div className="flex-row flex-wrap max-sm:items-center max-sm:justify-start justify-center gap-x-[4vw]  max-sm:gap-[4vw] gap-[2vw] flex w-full items-center">
                   {genAIBottom.map((tool, index) => (
                     <ToolItem key={index} tool={tool} />
                   ))}
@@ -202,8 +183,11 @@ export default function PresentationLayer() {
         </div>
       </div>
 
-      <div className="max-sm:w-[450vw] max-sm:py-[3vw] max-sm:rounded-full w-full flex items-center justify-center  bg-gradient-to-r from-white/0 to-white/4 border-white/20 rounded-[2vw] border">
-        <div className="h-auto mt-[.5vw] max-sm:w-[25vw] w-[8vw]">
+      <div
+        ref={UnifyRef}
+        className="w-full flex items-center justify-center bg-gradient-to-r from-white/0 to-white/4 border-white/20 rounded-[2vw] max-sm:rounded-[3vw] border"
+      >
+        <div className="h-auto max-sm:w-[25vw]  mt-[.5vw] w-[8vw]">
           <Image
             width={100}
             height={100}
@@ -216,3 +200,104 @@ export default function PresentationLayer() {
     </section>
   );
 }
+
+const usecaseLayer = [
+  "Customer Persistency",
+  "Fraud Detection",
+  "Demand Forecasting",
+  "Document Search",
+  "Content/Code Generation",
+  "Information Extraction",
+];
+
+const coreTools = [
+  {
+    name: "SmartEDA",
+    src: "/assets/icons/presentationLayer/left/smartEDATop.svg",
+  },
+  {
+    name: "FeatureCraft",
+    src: "/assets/icons/presentationLayer/left/featureCraft.svg",
+  },
+  {
+    name: "ModelCraft",
+    src: "/assets/icons/presentationLayer/left/modelCraft.svg",
+  },
+];
+
+const capabilities = [
+  {
+    name: "Data Integration Toolkit",
+    src: "/assets/icons/presentationLayer/left/dataIntegration.svg",
+  },
+  {
+    name: "Feature Store",
+    src: "/assets/icons/presentationLayer/left/featureStore.svg",
+  },
+  {
+    name: "Model Development",
+    src: "/assets/icons/presentationLayer/left/modelDevelopment.svg",
+  },
+  {
+    name: "SmartEDA",
+    src: "/assets/icons/presentationLayer/left/smartEDA.svg",
+  },
+  {
+    name: "Model Management & Monitoring",
+    src: "/assets/icons/presentationLayer/left/modelManagemnt.svg",
+  },
+  {
+    name: "Feedback Loop",
+    src: "/assets/icons/presentationLayer/left/feedbackLoop.svg",
+  },
+];
+
+const genAITop = [
+  {
+    name: "LLM Hub",
+    src: "/assets/icons/presentationLayer/right/llm.svg",
+  },
+  {
+    name: "Prompt Management",
+    src: "/assets/icons/presentationLayer/right/promptManagement.svg",
+  },
+  {
+    name: "Agent Management",
+    src: "/assets/icons/presentationLayer/right/agent.svg",
+  },
+  {
+    name: "Tool Hub",
+    src: "/assets/icons/presentationLayer/right/toolHUB1.svg",
+  },
+];
+
+const genAIBottom = [
+  {
+    name: "LLM Deployment",
+    src: "/assets/icons/presentationLayer/right/llmdeployment.svg",
+  },
+  {
+    name: "Tool Management",
+    src: "/assets/icons/presentationLayer/right/tool.svg",
+  },
+  {
+    name: "Vector DB",
+    src: "/assets/icons/presentationLayer/right/vector.svg",
+  },
+  {
+    name: "Data Indexing",
+    src: "/assets/icons/presentationLayer/right/dataindex.svg",
+  },
+  {
+    name: "Orchestrator",
+    src: "/assets/icons/presentationLayer/right/orchestrator.svg",
+  },
+  {
+    name: "Fine-Tuning",
+    src: "/assets/icons/presentationLayer/right/finetuning.svg",
+  },
+  {
+    name: "Guardrails",
+    src: "/assets/icons/presentationLayer/right/guardrails.svg",
+  },
+];
