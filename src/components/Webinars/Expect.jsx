910d890bb0ac0d3a59a0b1341c99ed5d516cf32a
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2025-09-22 18:56:03 +0530

    update
    webinar page responsive

--- src/components/Webinars/Expect.jsx
+++ src/components/Webinars/Expect.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Copy from "../Animations/Copy";
+import { lineAnim } from "../Animations/gsapAnimations";
 gsap.registerPlugin(ScrollTrigger);
 
 const POINTS = [
@@ -40,6 +41,7 @@ const POINTS = [
 
 export default function Expect() {
   const [isMobile, setIsMobile] = useState(false);
+  // lineAnim()
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -57,7 +59,7 @@ export default function Expect() {
 
   return (
     <section className="w-screen container relative z-[10]" id="expect">
-      <div className="w-full flex flex-col items-center justify-center gap-y-[5vw] max-sm:gap-y-[15vw]">
+      <div className="w-full flex flex-col items-center justify-center gap-y-[5vw] max-sm:gap-y-[20vw]">
         <div className="text-center space-y-[2vw] max-sm:space-y-[5vw]">
           <h2 className="text-90 headingAnim text-white-200 max-sm:text-center">
             What to Expect
@@ -68,22 +70,23 @@ export default function Expect() {
           {POINTS.map(({ id, text,title}) => (
             <div
               key={id}
-              className=" flex flex-col gap-[1.2vw] items-start about-item w-[44%] fadeup max-sm:w-full max-sm:items-center max-sm:gap-y-[4vw]"
+              className=" flex flex-col gap-[1.2vw] items-start about-item w-[44%] fadeup max-sm:w-full max-sm:items-center max-sm:gap-y-[7vw]"
             >
-              <div className="w-[15%] relative max-sm:w-[30%]">
-                <div className="relative w-[6.5vw] h-[6.5vw] border border-white-200 rounded-full flex items-center justify-center max-sm:w-[20vw] max-sm:h-[20vw]">
-                  <p className="about-id text-white-200 text-[1.5vw] font-head relative z-[1] max-sm:text-[4.2vw]">
+              <div className="w-[15%] relative max-sm:w-fit">
+                <div className="relative w-[6.5vw] h-[6.5vw] border border-white-200 rounded-full flex items-center justify-center max-sm:w-[18vw] max-sm:h-[18vw]">
+                  <p className="about-id text-white-200 text-[1.5vw] font-head relative z-[1] max-sm:text-[4.2vw] max-sm:text-center">
                     {id}
                   </p>
                 </div>
               </div>
-              <div className="space-y-[1.2vw]">
-                <p className="text-30  text-primary-2">
+              <div className="space-y-[1.2vw] max-sm:space-y-[7vw]">
+                <p className="text-30  text-primary-2 max-sm:text-center">
                   {title}
                 </p>
 
-                <p className="text-white-300">{text}</p>
+                <p className="text-white-300 max-sm:text-center">{text}</p>
               </div>
+              <span className="lineDraw w-full h-[1px] bg-white/20 hidden max-sm:block mt-[5vw]"></span>
             </div>
           ))}
         </div>
--- src/components/Webinars/UpcomingWebinars.jsx
+++ src/components/Webinars/UpcomingWebinars.jsx
@@ -6,140 +6,137 @@ import { headingAnim } from "../Animations/gsapAnimations";
 import Copy from "../Animations/Copy";
 import { LinkButton } from "../Button/LinkButton";
 
-
-
 const mainAnnouncement = {
-    image: "/assets/images/news/dsw-launches.png",
-    alt: "dsw-launches",
-    title:
-      "Deploying GenAI for Claims Automation",
-    description:
-      "Data Science Wizards (DSW) has unveiled a groundbreaking capability that allows insurance providers to deploy Generative AI solutions in just 2–4 hours, transforming traditional workflows such as claims processing, fraud detection, customer support, and document intelligence.",
-    format:"Live Demo + Q&A",
-    date: "July 4, 2025",
-    author: "Jane Smith",
-    href: "#",
-  };
+  image: "/assets/images/news/dsw-launches.png",
+  alt: "dsw-launches",
+  title: "Deploying GenAI for Claims Automation",
+  description:
+    "Data Science Wizards (DSW) has unveiled a groundbreaking capability that allows insurance providers to deploy Generative AI solutions in just 2–4 hours, transforming traditional workflows such as claims processing, fraud detection, customer support, and document intelligence.",
+  format: "Live Demo + Q&A",
+  date: "July 4, 2025",
+  author: "Jane Smith",
+  href: "#",
+};
 
-  const secondaryAnnouncements = [
-    {
-      image: "/assets/images/news/unifyai-achieves.png",
-      alt: "unifyai-achieves",
-      title: "Compliance-First AI: SOC 2, ISO & Beyond",
-      type:"Expert Panel",
-      date: "July 18, 2025",
-      href: "/",
-
-    },
-    {
-      image: "/assets/images/news/unifyai-achieves.png",
-      alt: "unifyai-achieves",
-      title:
-        "AI in Insurance: Roadmap to Production in 30 Days",
-        type:"Workshop",
-      date: "August 2, 2025",
-      href: "/",
-    },
-    {
-      image: "/assets/images/news/unifyai-achieves.png",
-      alt: "unifyai-achieves",
-      title:
-        "UnifyAI Showcase: What's New This Quarter",
-        type:"Product Walkthrough",
-      date: "August 16, 2025",
-      href: "/",
-    },
-  ];
-
+const secondaryAnnouncements = [
+  {
+    image: "/assets/images/news/unifyai-achieves.png",
+    alt: "unifyai-achieves",
+    title: "Compliance-First AI: SOC 2, ISO & Beyond",
+    type: "Expert Panel",
+    date: "July 18, 2025",
+    href: "/",
+  },
+  {
+    image: "/assets/images/news/unifyai-achieves.png",
+    alt: "unifyai-achieves",
+    title: "AI in Insurance: Roadmap to Production in 30 Days",
+    type: "Workshop",
+    date: "August 2, 2025",
+    href: "/",
+  },
+  {
+    image: "/assets/images/news/unifyai-achieves.png",
+    alt: "unifyai-achieves",
+    title: "UnifyAI Showcase: What's New This Quarter",
+    type: "Product Walkthrough",
+    date: "August 16, 2025",
+    href: "/",
+  },
+];
 
 const UpcomingWebinars = () => {
   return (
     <section className="container" id="announcement">
-        <div className="space-y-[7vw]">
-        <div className="w-1/2 space-[5vw]">
-      <h2 className=" text-100  headingAnim">
-       Upcoming Webinars & Events
-      </h2>
-      <Copy>
-        <p>See how DSW is making waves across global tech and AI publications. Explore press features, interviews with our leadership, and industry mentions.</p>
-      </Copy>
-      </div>
-      <div className="w-full space-y-[3vw]">
-        {/* Main Announcement */}
-        <div className="w-full space-y-[4vw]">
-          <div className="w-full h-fit flex gap-[4.5vw]">
-            <div className="w-[30vw] h-[23vw] rounded-[1.2vw] overflow-hidden fadeup">
-              <Image
-                src={mainAnnouncement.image}
-                alt={mainAnnouncement.alt}
-                className="w-full h-full"
-                width={400}
-                height={300}
-              />
-            </div>
-            <div className="w-[60%] flex flex-col gap-[2.5vw] mt-[1vw]">
-              <Copy>
-                <h3 className="text-30">{mainAnnouncement.title}</h3>
-              </Copy>
-              <Copy>
-                <p>{mainAnnouncement.description}</p>
-              </Copy>
-
-              <div className="flex gap-[4vw] fadeup">
-                <div className="flex flex-col gap-[0.5vw]">
-                  <p className="text-white/40">Format</p>
-                  <p>{mainAnnouncement.format}</p>
-                </div>
-                <div className="flex flex-col gap-[0.5vw]">
-                  <p className="text-white/40">Webinar Date</p>
-                  <p>{mainAnnouncement.date}</p>
-                </div>
-                <div className="flex flex-col gap-[0.5vw]">
-                  <p className="text-white/40">Author</p>
-                  <p>{mainAnnouncement.author}</p>
-                </div>
-              </div>
-
-              <div className="w-fit fadeup">
-                <WhiteButton
-                  background="border-primary-2 border bg-transparent hover:bg-transparent"
-                  circleColor={"bg-primary-2 group-hover:!bg-primary-2"}
-                  text="Register Now"
-                  href={mainAnnouncement.href}
-                  className="hover:text-primary-2 text-primary-2"
-                />
-              </div>
-            </div>
-          </div>
-          <span className="w-full h-[1px] block bg-white/20 lineDraw" />
+      <div className="space-y-[7vw]">
+        <div className="w-1/2 space-y-[1vw] max-sm:w-full max-sm:text-center max-sm:space-y-[7vw]">
+          <h2 className=" text-90  headingAnim">Upcoming Webinars & Events</h2>
+          <Copy>
+            <p>
+              See how DSW is making waves across global tech and AI
+              publications. Explore press features, interviews with our
+              leadership, and industry mentions.
+            </p>
+          </Copy>
         </div>
-
-        {/* Secondary Announcements */}
-        <div className="w-full flex justify-between fadeup pt-[3vw]">
-          {secondaryAnnouncements.map((item, index) => (
-            <div
-              key={index}
-              className="w-[32%] h-fit flex flex-col gap-[1.5vw]"
-            >
-              <div className="w-full h-[15vw] rounded-[1.2vw] overflow-hidden">
+        <div className="w-full space-y-[3vw] max-sm:mt-[20vw]">
+          {/* Main Announcement */}
+          <div className="w-full space-y-[4vw] max-sm:space-y-[7vw]">
+            <div className="w-full h-fit flex gap-[4.5vw] max-sm:flex-col max-sm:gap-y-[7vw]">
+              <div className="w-[30vw] h-[23vw] rounded-[1.2vw] overflow-hidden fadeup max-sm:w-full max-sm:h-[35vh]">
                 <Image
-                  src={item.image}
-                  alt={item.alt}
+                  src={mainAnnouncement.image}
+                  alt={mainAnnouncement.alt}
                   className="w-full h-full"
                   width={400}
                   height={300}
                 />
               </div>
-              <h3 className="text-30">{item.title}</h3>
-              <div className="flex justify-between w-full">
-              <p>{item.type}</p>
-              <p>{item.date}</p>
+              <div className="w-[60%] flex flex-col gap-[2vw] mt-[1vw] max-sm:w-full max-sm:gap-[7vw]">
+                <Copy>
+                  <h3 className="text-30">{mainAnnouncement.title}</h3>
+                </Copy>
+                <Copy>
+                  <p>{mainAnnouncement.description}</p>
+                </Copy>
+
+                <div className="flex gap-[4vw] fadeup max-sm:flex-wrap max-sm:justify-between">
+                  <div className="flex flex-col gap-[0.5vw]">
+                    <p className="text-white/40">Format</p>
+                    <p>{mainAnnouncement.format}</p>
+                  </div>
+                  <div className="flex flex-col gap-[0.5vw]">
+                    <p className="text-white/40">Webinar Date</p>
+                    <p>{mainAnnouncement.date}</p>
+                  </div>
+                  <div className="flex flex-col gap-[0.5vw]">
+                    <p className="text-white/40">Author</p>
+                    <p>{mainAnnouncement.author}</p>
+                  </div>
+                </div>
+
+                <div className="w-fit fadeup max-sm:my-[10vw]">
+                  <WhiteButton
+                    background="border-primary-2 border bg-transparent hover:bg-transparent"
+                    circleColor={"bg-primary-2 group-hover:!bg-primary-2"}
+                    text="Register Now"
+                    href={mainAnnouncement.href}
+                    className="hover:text-primary-2 text-primary-2"
+                  />
+                </div>
               </div>
-              <LinkButton text={"Register Now"} href={item.href} />
             </div>
-          ))}
+            <span className="w-full h-[1px] block bg-white/20 lineDraw" />
+          </div>
+
+          {/* Secondary Announcements */}
+          <div className="max-sm:w-screen overflow-x-scroll max-sm:ml-[-7vw] max-sm:mt-[7vw]">
+            <div className="w-full flex justify-between fadeup pt-[3vw] max-sm:w-[270vw] max-sm:px-[7vw] max-sm:pb-[10vw] ">
+              {secondaryAnnouncements.map((item, index) => (
+                <div
+                  key={index}
+                  className="w-[32%] h-fit flex flex-col gap-[1.5vw] max-sm:gap-[7vw]"
+                >
+                  <div className="w-full h-[15vw] rounded-[1.2vw] overflow-hidden max-sm:h-[25vh] max-sm:rounded-[4.5vw] max-sm:border max-sm:border-white/20">
+                    <Image
+                      src={item.image}
+                      alt={item.alt}
+                      className="w-full h-full"
+                      width={400}
+                      height={300}
+                    />
+                  </div>
+                  <h3 className="text-30">{item.title}</h3>
+                  <div className="w-full flex justify-between">
+                    <p>{item.type}</p>
+                    <p>{item.date}</p>
+                  </div>
+                  <LinkButton text={"Read More"} href={item.href} />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
       </div>
     </section>
   );
